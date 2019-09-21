8e49207b81e8c34913425a025defd237eb5c8ee4
pandabearcoder <vlouie00@gmail.com>
2019-09-21 13:46:42 +0800

    Add basic exercises

---
+++ basic/exercise2.py
@@ -0,0 +1,14 @@
+# If Statements and Comments Exercise
+
+# Make an if-else statement if year is between 2000 and 2100
+# (including both numbers), then print out:
+# "Welcome to the 21st century"
+# Else print out:
+# "You are before or after the 21st century"
+
+year = 1830
+
+if 2100 >= year >= 2000:
+    print('Welcome to the 21st century')
+else:
+    print('You are before or after the 21st century')
